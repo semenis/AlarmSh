ee25129a22267b25a88198fa5d3f4fa057b4d386
Семен <semen.say@ya.ru>
2018-07-26 08:19:27 +0300

    City instructions
    Add a new var and new comment with instruction

--- weather.py
+++ weather.py
@@ -1,5 +1,7 @@
 import requests, bs4
-s=requests.get('https://sinoptik.com.ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B0%D0%BB%D1%83%D0%B3%D0%B0-100553915')
+# Find your sity in sinoptik.com and past link, default it is Kaluga
+link = 'https://sinoptik.com.ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D0%B0%D0%BB%D1%83%D0%B3%D0%B0-100553915'
+s=requests.get(link)
 b=bs4.BeautifulSoup(s.text, "html.parser")
 p=b.select('.rSide .description')
 pogoda=p[0].getText()
